abdbc3a49e2bf0a04c97dce92863629c9e7c8c3f
Youssef Badreldin <youssefmbadreldin@gmail.com>
2025-05-10 01:58:26 +0300

    final

--- src/views/admin/events.vue
+++ src/views/admin/events.vue
@@ -303,7 +303,7 @@
                 <polyline points="7 10 12 15 17 10"></polyline>
                 <line x1="12" y1="15" x2="12" y2="3"></line>
               </svg>
-              Export to CSV
+              Export Data
             </button>
             <button @click="closeParticipantsModal" class="cancel-btn">Close</button>
           </div>
